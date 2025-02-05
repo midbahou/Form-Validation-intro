14c1c6370013beb5de1f36fd750824a08c2b5956
midbahou <midbahou@gmail.com>
2025-02-05 16:00:29 -0500

    local Storage added

--- script.js
+++ script.js
@@ -46,3 +46,37 @@ form.addEventListener('submit', function(e){
 
     console.log(`Sending data ....`);
 });
+
+// get a value into the localstorage
+localStorage.setItem("email", "user1@test.com")
+localStorage.setItem("user", "user1")
+
+// get a value by passing the key name
+let loggedInUser = localStorage.getItem("user");
+console.log(loggedInUser);
+
+// remove a value by passing the key
+localStorage.removeItem("email")
+
+// localStorage.clear()
+
+
+localStorage.setItem("age", 29)
+
+let arr = ["user99", "user88"]
+
+// console.log(JSON.stringify(arr))
+
+localStorage.setItem("players", JSON.stringify(arr))
+
+let players = JSON.parse(localStorage.getItem("players"));
+console.log(players[1]);
+
+
+
+if (localStorage.getItem("cart")) {
+    console.log(localStorage.getItem("cart"));
+} else {
+    console.log("item not found");
+    localStorage.setItem("cart", JSON.stringify(["apples", "keyboard"]))
+}
